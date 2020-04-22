96df3609aa7d8675b145e334cd56e80fb059395b
sakshat <voteforsakshat@gmail.com>
2020-04-23 03:36:29 +0530

    edited readme.md

---
+++ build/precache-manifest.4aa83b006527db9065da5638443a563e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10ad5dbbf56d923b92ab02ded887abb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b0d78bd27893f2fca32",
+    "url": "/static/css/main.40531fd6.chunk.css"
+  },
+  {
+    "revision": "050432e7ebd0a4e7d434",
+    "url": "/static/js/2.5f5de30a.chunk.js"
+  },
+  {
+    "revision": "b013b7cd7a2da92c16a2d9932cfeb76c",
+    "url": "/static/js/2.5f5de30a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b0d78bd27893f2fca32",
+    "url": "/static/js/main.41249eb5.chunk.js"
+  },
+  {
+    "revision": "5d8d9fa9711a03083750",
+    "url": "/static/js/runtime-main.6d3229b6.js"
+  }
+]);
